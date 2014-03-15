2bf708220ba3c0da58a123c89b46b832c4cdd218
Tate Bosler <tatebosler@gmail.com>
2014-03-15 14:36:34 -0500

    Google login fixes

--- login/index.php
+++ login/index.php
@@ -3,91 +3,99 @@ require_once __DIR__.'/vendor/autoload.php';
 require_once 'tokens.php';
 session_start();
 
-$fb_config = array(
-	'appId' => $fb_app_id,
-	'secret' => $fb_app_secret,
-);
-$facebook = new Facebook($fb_config);
-$fb_user = $facebook->getUser();
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+const CLIENT_ID = $google_client_id;
+const CLIENT_SECRET = $google_app_secret;
+const APPLICATION_NAME = $google_app_name;
 
 $client = new Google_Client();
-$client->setApplicationName($google_app_name);
-$client->setClientId($google_client_id);
-$client->setClientSecret($google_app_secret);
-$client->setRedirectUri($google_app_redirect);
-$client->setDeveloperKey($google_app_key);
+$client->setApplicationName(APPLICATION_NAME);
+$client->setClientId(CLIENT_ID);
+$client->setClientSecret(CLIENT_SECRET);
+$client->setRedirectUri('postmessage');
+
 $plus = new Google_PlusService($client);
 
-if(isset($_GET['logout'])) {
-	# sign out of iFF
-	setcookie('iff-id',0,1,'/','.ifantasyfitness.com');
-	setcookie('iff-google',0,1,'/','.ifantasyfitness.com');
-	setcookie('iff-facebook',0,1,'/','.ifantasyfitness.com');
-	
-	# sign out of Facebook, if the user is signed in
-	if($fb_user) {
-		$fb_logout = $facebook->getLogoutUrl(array('next', "http://www.ifantasyfitness.com/login?logout_google"));
-		header("Location: $fb_logout");
-	} else {
-		$_GET['input_google'] = true;
-	}
-}
+$app = new Silex\Application();
+$app['debug'] = true;
 
-if(isset($_GET['logout_google'])) {
-	# sign out of Google
-	echo '<div onload="gapi.auth.signOut();"></div>
-	<div onload="window.location.assign("http://www.ifantasyfitness.com/logout?ok");"></div>';
-}
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__,
+));
+$app->register(new Silex\Provider\SessionServiceProvider());
 
-// if already signed in, bypass
-if(isset($_COOKIE['iff-id'])) header('Location: http://www.ifantasyfitness.com/home');
+// Initialize a session for the current user, and render index.html.
+$app->get('/', function () use ($app) {
+    $state = md5(rand());
+    $app['session']->set('state', $state);
+    return $app['twig']->render('index.html', array(
+        'CLIENT_ID' => CLIENT_ID,
+        'STATE' => $state,
+        'APPLICATION_NAME' => APPLICATION_NAME
+    ));
+});
 
-// Google based Login
-if(isset($_GET['code'])) {
-	$code = $_GET['code'];
-	echo $code;
-	echo "Trying to authenticate.";
-	/* $client->authenticate($code);
-	$me = $plus->people->get('me');
-	setcookie('g-uuid',$me['id'],0,'/','.ifantasyfitness.com');
-	setcookie('g-first',$me['name']['givenName'],0,'/','.ifantasyfitness.com');
-	setcookie('g-last',$me['name']['familyName'],0,'/','.ifantasyfitness.com');
-	header('Location: http://www.ifantasyfitness.com/setup/google'); */
-}
+// Upgrade given auth code to token, and store it in the session.
+// POST body of request should be the authorization code.
+// Example URI: /connect?state=...&gplus_id=...
+$app->post('/connect', function (Request $request) use ($app, $client) {
+    $token = $app['session']->get('token');
 
-// Facebook based Login
-if($fb_user) {
-	try {
-		$user_profile = $facebook->api('/me','GET');
-		setcookie('f-uuid',$user_profile['id'],0,'/','.ifantasyfitness.com');
-		setcookie('f-first',$user_profile['first_name'],0,'/','.ifantasyfitness.com');
-		setcookie('f-last',$user_profile['last_name'],0,'/','ifantasyfitness.com');
-		header('Location: http://www.ifantasyfitness.com/setup/facebook');
-	} catch(FacebookApiException $e) {
-		$login_url = $facebook->getLoginUrl();
-	}   
-} else {
-	$login_url = $facebook->getLoginUrl();
-}
+    if (empty($token)) {
+        // Ensure that this is no request forgery going on, and that the user
+        // sending us this connect request is the user that was supposed to.
+        if ($request->get('state') != ($app['session']->get('state'))) {
+            return new Response('Invalid state parameter', 401);
+        }
 
-$title = 'Sign in';
-include('../php/head.php');
-?>
-<div class="row">
-	<div class="col-xs-12">
-		<h1>Sign in</h1>
-	</div>
-</div>
-<div class="row">
-	<div class="col-md-8">
-		<p class="lead">Please choose a social network to sign in with. You can add or remove additional networks at any time.</p>
-	</div>
-	<div class="col-md-4">
-		<p><a href="<?=$login_url?>" class="btn btn-default btn-block" style="background:#3b5998;border:1px solid #3b5998;"><i class="fa fa-facebook"></i> Sign in with Facebook</a></p>
-		<!-- <p><a href="#" class="btn btn-default btn-block" style="background:#55acee;border:1px solid #55acee;"><i class="fa fa-twitter"></i> Sign in with Twitter</a></p> Coming Soon! -->
-		<p><a href="https://accounts.google.com/o/oauth2/auth?scope=profile&redirect_uri=http%3A%2F%2Fwww.ifantasyfitness.com%2Flogin&response_type=code&client_id=7336321947-ublsm7i9aa19ae7bn9fsvjeia3qudj3k.apps.googleusercontent.com" class="btn btn-default btn-block" style="background:#dd4b39;border:1px solid #dd4b39;"><i class="fa fa-google-plus"></i> Sign in with Google</a></p>
-	</div>
-</div>
-<?php
-include('../php/foot.php');
+        // Normally the state would be a one-time use token, however in our
+        // simple case, we want a user to be able to connect and disconnect
+        // without reloading the page.  Thus, for demonstration, we don't
+        // implement this best practice.
+        // $app['session']->set('state', '');
+
+        $code = $request->getContent();
+        // Exchange the OAuth 2.0 authorization code for user credentials.
+        $client->authenticate($code);
+        $token = json_decode($client->getAccessToken());
+
+        // You can read the Google user ID in the ID token.
+        // "sub" represents the ID token subscriber which in our case
+        // is the user ID. This sample does not use the user ID.
+        $attributes = $client->verifyIdToken($token->id_token, CLIENT_ID)
+            ->getAttributes();
+        $gplus_id = $attributes["payload"]["sub"];
+
+        // Store the token in the session for later use.
+        $app['session']->set('token', json_encode($token));
+        $response = 'Successfully connected with token: ' . print_r($token, true);
+    }
+
+    return new Response($response, 200);
+});
+
+// Get list of people user has shared with this app.
+$app->get('/people', function () use ($app, $client, $plus) {
+    $token = $app['session']->get('token');
+
+    if (empty($token)) {
+        return new Response('Unauthorized request', 401);
+    }
+
+    $client->setAccessToken($token);
+    $people = $plus->people->listPeople('me', 'visible', array());
+    return $app->json($people);
+});
+
+// Revoke current user's token and reset their session.
+$app->post('/disconnect', function () use ($app, $client) {
+    $token = json_decode($app['session']->get('token'))->access_token;
+    $client->revokeToken($token);
+    // Remove the credentials from the user's session.
+    $app['session']->set('token', '');
+    return new Response('Successfully disconnected', 200);
+});
+
+$app->run();
 ?>
