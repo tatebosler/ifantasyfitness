6c3ced77460f166dce0ed1bedc92a49f8054e011
ichiefboz <tatebosler@gmail.com>
2014-05-13 09:51:48 -0500

    Added forms

--- setup/register.php
+++ setup/register.php
@@ -1,6 +1,5 @@
 <?php
 if(!isset($_COOKIE['iff-id'])) header('Location: http://www.ifantasyfitness.com');
-if(!isset($_GET['season'])) header("Location: http://www.ifantasyfitnes.com/home");
 include('../php/db.php');
 $id = filter_var($_COOKIE['iff-id'], FILTER_SANITIZE_NUMBER_INT);
 $slug = filter_var($_GET['season'], FILTER_SANITIZE_SPECIAL_CHARS);
@@ -14,4 +13,195 @@ if(mysqli_num_rows($check_q) > 0) {
 	if(isset($_COOKIE['iff-facebook']) and $_COOKIE['iff-facebook'] === $user['facebook']) $valid = true;
 	if(!$valid) header('Location: http://www.ifantasyfitness.com');
 }
+
+if(isset($_POST['submitted'])) {
+	# first process profile changes
+	$query = "UPDATE users SET ";
+	if($_POST['first'] != $user['first']) {
+		$first = filter_var($_POST['first'], FILTER_SANITIZE_SPECIAL_CHARS);
+		$user['first'] = filter_var($_POST['first'], FILTER_SANITIZE_SPECIAL_CHARS);
+		$query .= "first='$first', ";
+	}
+	if($_POST['last'] != $user['last']) {
+		$last = filter_var($_POST['last'], FILTER_SANITIZE_SPECIAL_CHARS);
+		$user['last'] = filter_var($_POST['last'], FILTER_SANITIZE_SPECIAL_CHARS);
+		$query .= "last='$last', ";
+	}
+	if($_POST['grad'] != $user['grad']) {
+		$grad = filter_var($_POST['grad'], FILTER_SANITIZE_NUMBER_INT);
+		$user['grad'] = filter_var($_POST['grad'], FILTER_SANITIZE_NUMBER_INT);
+		$query .= "grad=$grad, ";
+	}
+	if($_POST['gender'] != $user['gender']) {
+		$gender = filter_var($_POST['gender'], FILTER_SANITIZE_NUMBER_INT);
+		$user['gender'] = filter_var($_POST['gender'], FILTER_SANITIZE_NUMBER_INT);
+		$query .= "gender=$gender, ";
+	}
+	if(!empty($user['first']) and !empty($user['last']) and ($user['gender'] <= 1) and ($user['grad'] >= 1900)) {
+		# Profile is complete.
+		$user['profile'] = 0;
+		$query .= "profile=0";
+	} else {
+		$user['profile'] = 1;
+		$query .= "profile=1";
+	}
+	$query .= " WHERE id=$id";
+	
+	# then process team registration ONLY IF profile is OK.
+	if($user['profile'] == 0) {
+		
+	}
+}
+$title = "Welcome";
+include('../php/head-auth.php');
+?>
+<div class="row">
+	<div class="col-xs-12">
+		<form name="profile" class="form-horizontal" method="post">
+			<h4>Account information</h4>
+			<div class="form-group">
+				<label class="col-xs-2 control-label">First name</label>
+				<div class="col-xs-10">
+					<input type="text" name="first" class="form-control" value="<?=$user['first']?>">
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-xs-2 control-label">First name</label>
+				<div class="col-xs-10">
+					<input type="text" name="first" class="form-control" value="<?=$user['first']?>">
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-xs-2 control-label">Last name</label>
+				<div class="col-xs-10">
+					<input type="text" name="last" class="form-control" value="<?=$user['last']?>">
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-xs-2 control-label">Graduation</label>
+				<div class="col-xs-10">
+					<input type="text" name="grad" class="form-control" value="<?=$user['grad']?>">
+					<span class="help-block">The year you graduated (or will graduate) from high school</span>
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-xs-2 control-label">Gender</label>
+				<div class="col-xs-10">
+					<label class="radio-inline">
+						<input type="radio" name="gender" value="0" <?php if($user['gender'] == 0) echo 'checked' ?>> <i class="fa fa-male"></i> Male
+					</label>
+					<label class="radio-inline">
+						<input type="radio" name="gender" value="1" <?php if($user['gender'] == 1) echo 'checked' ?>> <i class="fa fa-female"></i> Female
+					</label>
+				</div>
+			</div>
+			<?php
+			# Grab seasons available for registration
+			$now = time();
+			$season_grabber = @mysqli_query($db, "SELECT * FROM seasons WHERE reg_start <= $now AND reg_end >= $now");
+			if(mysqli_num_rows($season_grabber) > 0) {
+				# There are seasons available.
+				echo '<h4>Season registration</h4>';
+				if(mysqli_num_rows($season_grabber) == 1) {
+					# Juse one season. :)
+					$season = mysqli_fetch_array($season_grabber);
+					echo '<p class="lead">You are registering for the '.$season['name'].' season.</p>
+					<input type="hidden" name="season" value="'.$season['name'].'">';
+				} else {
+					echo '<div class="form-group">
+					<label class="col-xs-6 control-label">Please choose a season to register for. You can register for more at any time.</label>
+					<div class="col-xs-6">';
+					while($season = mysqli_fetch_array($season_grabber)) {
+						echo '<div class="radio">
+						<label><input type="radio" name="season" value="'.$season['name'].'">'.$season['display_nane'].'</label></div>';
+					}
+					echo '</div>'
+				}
+				echo '<div class="form-group">
+				<label class="col-xs-2 control-label">Prediction</label>
+					<div class="col-xs-10">
+						<p class="form-control-static">Please enter your prediction for <strong>how many TOTAL points you will score this season.</strong> This will also serve as your season point goal, and you can view it on your <a href="/home">home screen</a>. Captains and coaches will see this value when drafting you to a team, so please enter a realistic prediction.</p><br>
+						<input type="number" name="prediction" class="form-control" min="0" max="5000">
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="col-xs-2 control-label">Division</label>
+					<div class="col-xs-10">
+						<p class="form-control-static">Please select a division for the leaderboards.</p>
+						<div class="radio">
+							<label>
+								<input type="radio" name="division" value="1"><strong>Upperclassmen</strong> - students entering junior or senior year
+						</label>
+					</div>
+					<div class="radio">
+						<label>
+							<input type="radio" name="division" value="2">
+							<strong>Underclassmen</strong> - students entering freshman or sophomore year
+						</label>
+					</div>
+					<div class="radio">
+						<label>
+							<input type="radio" name="division" value="3">
+							<strong>Middle School</strong> - students entering grades 6 through 8
+						</label>
+					</div>
+					<div class="radio">
+						<label>
+							<input type="radio" name="division" value="4">
+							<strong>Staff</strong> - all SPPS staff, including coaches
+						</label>
+					</div>
+					<div class="radio">
+						<label>
+							<input type="radio" name="division" value="5">
+							<strong>Parents</strong> of runners or skiers (who aren\'t SPPS staff)
+						</label>
+					</div>
+					<div class="radio">
+						<label>
+							<input type="radio" name="division" value="6" >
+							<strong>Alumni</strong> of Highland (who don\'t fall into a division above)
+						</label>
+					</div>
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-xs-2 control-label">Daily Goals</label>
+				<div class="col-xs-10">
+					<p class="form-control-static">Please select a Daily Running Goal program. This will appear under your season points goal, and give you an idea of what to do to improve your running ability.</p>
+					<div class="radio">
+						<label>
+							<input type="radio" name="goal" value="0">
+							<strong>Rookie</strong> - Choose if you are new to the team, or have been on for one year or less.
+						</label>
+					</div>
+					<div class="radio">
+						<label>
+							<input type="radio" name="goal" value="1">
+							<strong>JV</strong> - Choose if you have been on the team for more than one year, or are recovering from an injury preventing you from training at Varsity.
+						</label>
+					</div>
+					<div class="radio">
+						<label>
+							<input type="radio" name="goal" value="2">
+							<strong>Varsity</strong> - Choose if you have been running varsity for more than one year, or have been on the team for more than two years.
+						</label>
+					</div>
+					<div class="radio">
+						<label>
+							<input type="radio" name="goal" value="3">
+							<strong>Elite</strong> - Choose if you want to run at State. Warning: requires serious motivation.
+						</label>
+					</div>
+					<br>
+					<input type="submit" class="btn btn-primary" value="Register">
+				</div>
+			</div>';
+			}
+			?>
+		</form>
+	</div>
+</div>
+<?php
+include('../php/foot.php');
 ?>
