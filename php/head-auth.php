f1ef8ba9fd22e88ad4f22ff80ea22aebc29bd015
Tate Bosler <tatebosler@gmail.com>
2014-05-18 18:38:39 -0500

    Updated Font Awesome to 4.1.0

--- php/head-auth.php
+++ php/head-auth.php
@@ -6,7 +6,7 @@ echo '<!DOCTYPE html>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>'.$title.' - iFantasyFitness - Train Competitively</title>
 	    <link href="//ifantasyfitness.com/css/main.css" rel="stylesheet">
-	    <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+	    <link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.css" rel="stylesheet">
 	    <link href="//ifantasyfitness.com/css/bootstrap-datetimepicker.css" rel="stylesheet">
 	</head>
     <body>
