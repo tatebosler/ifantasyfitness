eb16baf07ccae711897c08458049a0091866298c
Tate Bosler <tatebosler@gmail.com>
2014-05-18 17:43:13 -0500

    Ok - debug all done!

--- teamscores.php
+++ teamscores.php
@@ -1,5 +1,5 @@
 <?php
-header("Location: http://www.ifantasyfitness.com"); # No humans!
+// header("Location: http://www.ifantasyfitness.com"); # No humans!
 require('php/db.php');
 
 # For teams with id > 1: Update the team score.
@@ -9,10 +9,10 @@ $checked_teams = array(); # Performance reasons
 while($person = mysqli_fetch_array($people_update)) {
 	$tid = $person['team'];
 	if(!in_array($tid,$checked_teams)) {
-		$checked_teams[] = $tid; # Don't check teams twice - Improves performance
+		// $checked_teams[] = $tid; # Don't check teams twice - Improves performance
 		$total = 0;
 		$run = 0;
-		$record_grab = @mysqli_query($db, "SELECT * FORM records WHERE team=$tid");
+		$record_grab = @mysqli_query($db, "SELECT * FROM records WHERE team=$tid");
 		while($record = mysqli_fetch_array($record_grab)) {
 			$total += $record['total'];
 			$run += $record['run_p'] + $record['run_team_p']; 
