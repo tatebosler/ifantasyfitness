94753fa3f25f7f00a670c8c7d40864cb39c3ba91
Tate Bosler <tatebosler@gmail.com>
2014-05-18 17:32:24 -0500

    Cron job to update team scores

---
+++ teamscores.php
@@ -0,0 +1,24 @@
+<?php
+header("Location: http://www.ifantasyfitness.com") # No humans!
+include('php/db.php');
+
+# For teams with id > 1: Update the team score.
+$people_update = @mysqli_query($db, "SELECT * FROM tMembers WHERE flag = 1");
+$checked_teams = array(); # Performance reasons
+
+while($person = mysqli_fetch_array($people_update)) {
+	$tid = $person['team'];
+	if(!in_array($tid,$checked_teams)) {
+		$checked_teams[] = $tid; # Don't check teams twice - Improves performance
+		$team_grab = @mysqli_query($db, "SELECT * FORM records WHERE team=$tid");
+		$total = 0;
+		$run = 0;
+		while($record = mysqli_fetch_array($team_grab)) {
+			$total += $record['total'];
+			$run += $record['run_p'] + $record['run_team_p']; 
+		}
+		$team_update = @mysqli_query($db, "UPDATE tData SET total=$total, running=$run WHERE id=$tid");
+		$deflag = @mysqli_query($db, "UPDATE tMembers SET flag=0 WHERE team=$tid");
+	}
+}
+?>
