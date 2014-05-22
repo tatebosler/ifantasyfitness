c125e886a0c91ffacff6b9e77a7ed1e9154971f3
Tate Bosler <tatebosler@gmail.com>
2014-05-22 17:33:10 -0500

    Woot more rules :D

--- rules/index.php
+++ rules/index.php
@@ -16,7 +16,9 @@ if(isset($_COOKIE['iff-id'])) {
 	$id = 0;
 }
 
-$title = 'Leaderboard';
+$stars = array("None", "Bronze", "Silver", "Gold", "Platinum", "Diamond");
+
+$title = 'Rules of the Game';
 $connected = true;
 if($valid) {
 	include('../php/head-auth.php');
@@ -32,7 +34,7 @@ if($valid) {
 		</div>
 		<h2>What Counts</h2>
 		<p><strong>Generally, anything that requires you to change clothes and results in a high heart rate.</strong> This includes things like aerobically-intensive sports, strength training, fitness classes, biking, and even power walks. Just make sure that you're getting a good workout when you count your points.</p>
-		<p>It does <strong>not</strong> include low-intensity activity like household chores, short walks, wrestling with younger cousins, or your commute between classes, nor does it include non-aerobic activity like dancing.</p>
+		<p>It does <strong>not</strong> include low-intensity activity like household chores, short walks, wrestling with younger cousins, or your commute between classes, nor does it include non-aerobic activity like dancing, golf, or Wii Fit.</p>
 		<p>The point is: you should only give yourself points if you believe that you got a good workout in.</p>
 		<h2>Miles to Points Conversion</h2>
 		<p>Everything you do gets converted to points. The current conversions are:</p>
@@ -70,11 +72,57 @@ if($valid) {
 		<?php
 		$alt_fetch = @mysqli_query($db, "SELECT * FROM globals WHERE name LIKE 'alt\_%' ORDER BY value ASC");
 		while($alt = mysqli_fetch_array($alt_fetch)) {
-			echo '<li><strong>'.$alt['display'].'</strong>: '.$alt['value'].'&times; multiplier of your total score</li>';
+			echo '<li><strong>'.$alt['display'].':</strong> '.$alt['value'].'&times; multiplier of your total score</li>';
 		}
 		?>
 		</ul>
 		<p><strong>Note:</strong> coaches may ask you for verification of your altitude, so we suggest taking photos or using a GPS watch while you're up there.</p>
+		<h2>Caps and Limits</h2>
+		<p>There is only one "true" limit: be honest with what you log. We generally don't have an issue, but please don't put in more than what you actually did. If in doubt, round down.</p>
+		<p>Some activities are limited, or "capped", at certain thresholds. The current caps are:</p>
+		<ul>
+		<?php
+		$cap_fetch = @mysqli_query($db, "SELECT * FROM globals WHERE name LIKE 'cap\_week\_%' ORDER BY value ASC");
+		while($cap = mysqli_fetch_array($cap_fetch)) {
+			echo '<li><strong>'.$cap['display'].':</strong> '.$cap['value'].' points per week</li>';
+		}
+		?>
+		</ul>
+		<p>For iFantasyFitness, one week is 7 consecutive days. We check and clear cap data every night.</p>
+		<h2>What You Get</h2>
+		<p>Aside from a better athletic base, iFantasyFitness has a couple awards that you can be eligible for:</p>
+		<ul>
+			<li><strong>Top individual:</strong> be in the top two men/women of your division. (This is for Upperclassmen and Underclassmen divisions only)</li>
+			<li><strong>Top team:</strong> be on the winning team.</li>
+			<li><strong>Distance awards:</strong> run a lot! Thresholds are below.</li>
+		</ul>
+		<p>We hand out awards at the August Team BBQ at the end of the season.</p>
+		<h3>Distance awards</h3>
+		<p>Distance stars are awarded to people that run a lot. You'll get a certificate if you reach Gold, a t-shirt if you reach Platinum, and a bar for your letter jacket if you reach Diamond. Pretty sweet stuff. The thresholds are:</p>
+		<div class="row">
+			<div class="col-xs-6">
+				<h4>Men</h4>
+				<ul>
+				<?php
+				$mStar_fetch = @mysqli_query($db, "SELECT * FROM globals WHERE name LIKE 'da-m-%' ORDER BY value ASC");
+				while($mStar = mysqli_fetch_array($mStar_fetch)) {
+					echo '<li><strong>'.$stars[$mStar['display']].':</strong> '.$mStar['value'].' miles</li>';
+				}
+				?>
+				</ul>
+			</div>
+			<div class="col-xs-6">
+				<h4>Women</h4>
+				<ul>
+				<?php
+				$mStar_fetch = @mysqli_query($db, "SELECT * FROM globals WHERE name LIKE 'da-f-%' ORDER BY value ASC");
+				while($mStar = mysqli_fetch_array($mStar_fetch)) {
+					echo '<li><strong>'.$stars[$mStar['display']].':</strong> '.$mStar['value'].' miles</li>';
+				}
+				?>
+				</ul>
+			</div>
+		</div>
 	</div>
 </div>
 <?php
