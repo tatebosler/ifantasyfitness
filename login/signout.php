34467812d4a6c4a3a8892085766df3e0a58118e7
Tate Bosler <tatebosler@gmail.com>
2014-05-26 20:55:39 -0500

    Trying to do sign out properly (bah, javascript)

--- login/signout.php
+++ login/signout.php
@@ -1,16 +1,9 @@
 <?php
 # Sign out of iFF
-$cookies = array('id','facebook','twitter','google','exp');
-foreach($cookies as $type) {
-	setcookie("iff-".$type, 0, 1, '/', '.ifantasyfitness.com');
+$names = array();
+foreach($_COOKIE as $name => $value) {
+	setcookie($name, 0, 16, '/', '.ifantasyfitness.com');
+	setcookie($name, 0, 16, '/', '.www.ifantasyfitness.com');
 }
-
-# sign out of Facebook first
-require_once __DIR__.'/vendor/autoload.php';
-require_once 'tokens.php';
-
-$fb_config = array("appId" => $fb_app_id, "secret" => $fb_app_secret);
-$facebook = new Facebook($fb_config);
-$fb_logout = $facebook->getLogoutUrl(array("next" => "http://www.ifantasyfitness.com/login"));
-header("Location: $fb_logout");
+header("Location: http://www.ifantasyfitness.com/");
 ?>
