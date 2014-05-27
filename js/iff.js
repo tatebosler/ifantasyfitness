8554917d0f3db18fb766be2e3700a9dc4ff54887
ichiefboz <tatebosler@gmail.com>
2014-05-27 09:52:18 -0500

    Fixed comments and prepared for more functions

--- js/iff.js
+++ js/iff.js
@@ -1,7 +1,11 @@
-# A JavaScript file for various random things around iFF.
+// A JavaScript file for various random things around iFF.
 
 $("#toggle-daily-goals").click(function() {
 	$("#daily-goal-data").toggle("slow", function() {
-		# Toggles visibility of the daily goals data.
+		// Toggles visibility of the daily goals data.
 	});
+});
+
+$(".populate-form").click(function () {
+	// Populates the quick-add with workout data.
 });
